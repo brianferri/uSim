4b7aba7f4f87fec6c0c1ddcea54c3e29a57253b8
Brian Ferri <me@brianferri.dev>
2025-05-04 18:57:42 +0200

    feat: add mock interaction function interface

--- src/physics/String.zig
+++ src/physics/String.zig
@@ -3,6 +3,18 @@ const std = @import("std");
 const Self = @This();
 
 tension: f64,
-vibrational_modes: f64,
-charge: f64,
-spin: f64,
+vibration_mode: f64,
+phase: f64,
+
+pub fn interact(self: *Self, other: *Self) void {
+    const avg_mode = (self.vibration_mode * other.tension +
+        other.vibration_mode * self.tension) /
+        (self.tension + other.tension);
+
+    self.vibration_mode = avg_mode;
+    other.vibration_mode = avg_mode;
+
+    const rand = std.crypto.random;
+    self.phase += rand.float(f64) * 0.1;
+    other.phase += rand.float(f64) * 0.1;
+}
